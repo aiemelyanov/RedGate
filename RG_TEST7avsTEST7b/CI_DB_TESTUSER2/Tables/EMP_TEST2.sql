bf566453cec2316db5c6b5b78512631b1541d87d
Andrey Emelyanov <aemelyanov@qtrade.ca>
2017-05-09 18:00:08 -0700

    corrected manually to test change by one person

--- RG_TEST7avsTEST7b/CI_DB_TESTUSER2/Tables/EMP_TEST2.sql
+++ RG_TEST7avsTEST7b/CI_DB_TESTUSER2/Tables/EMP_TEST2.sql
@@ -1,5 +1,7 @@
 CREATE TABLE ci_db_testuser2.emp_test2 (
-  "ID" NUMBER(22,2) NOT NULL,
-  "NAME" VARCHAR2(100 BYTE),
+  "ID" NUMBER(8,2) NOT NULL,
+  first_name VARCHAR2(100 BYTE) NOT NULL,
+  last_name VARCHAR2(250 BYTE),
+  salary NUMBER(5,2),
   CONSTRAINT emp_test2_pk PRIMARY KEY ("ID")
 );
