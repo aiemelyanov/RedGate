3e47f6de0f5a230e3072aefc9e31863e0e1c8177
Andrey Emelyanov <aemelyanov@qtrade.ca>
2017-05-09 17:27:28 -0700

    init_ shouldn't the same but the application found something

---
+++ RG_TEST7avsTEST7b/CI_DB_TESTUSER1/Tables/EMP_TEST1.sql
@@ -0,0 +1,5 @@
+CREATE TABLE ci_db_testuser1.emp_test1 (
+  "ID" NUMBER(22,2) NOT NULL,
+  "NAME" VARCHAR2(100 BYTE),
+  CONSTRAINT emp_test1_pk PRIMARY KEY ("ID")
+);
---
+++ RG_TEST7avsTEST7b/CI_DB_TESTUSER2/Tables/EMP_TEST2.sql
@@ -0,0 +1,5 @@
+CREATE TABLE ci_db_testuser2.emp_test2 (
+  "ID" NUMBER(22,2) NOT NULL,
+  "NAME" VARCHAR2(100 BYTE),
+  CONSTRAINT emp_test2_pk PRIMARY KEY ("ID")
+);
